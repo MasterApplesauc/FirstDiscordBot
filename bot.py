de2689c5f291fd65f809fb77fafe22cf7a833030
MasterApplesauc <joe.vincent96@gmail.com>
2021-03-03 13:17:55 -0800

    Added welcome message to new users, local message to indicate discord connection
    Pesky intents=intents tripping me up. Trouble figuring out why bot couldn't see new members after switching from Client.event to Bot.event

--- bot.py
+++ bot.py
@@ -6,15 +6,25 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL = os.getenv('DISCORD_CHANNEL')
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 #Vars
 global amongUsRoom 
 amongUsRoom = '00000'
+
+@bot.event
+async def on_ready():
+	print(f'{bot.user.name} has connected to Discord')
+
+@bot.event
+async def on_member_join(member):
+	print('a member has joined.')
+	await member.send('yo!')
 
 @bot.command()
 async def getRoom(ctx):
