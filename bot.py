73c878fd1d33d89ff05024674b0cfd0ef3d21c26
MasterApplesauc <joe.vincent96@gmail.com>
2021-03-03 14:21:52 -0800

    Added a 'happy birthday' event flow
    Sometimes nice, sometimes a little annoying, I've added in a few choices for the bot to wish someone a happy birthday on their day.

--- bot.py
+++ bot.py
@@ -23,6 +23,27 @@ async def on_ready():
 	print(f'{bot.user.name} has connected to Discord')
 
 @bot.event
+async def on_message(message):
+	channel = bot.get_channel(int(os.getenv('DISCORD_CHANNEL')))
+	if message.author == bot.user:
+		return
+
+	happyBirthdayMessages = [
+		'Happy Birthday!',
+		'Happy Birthday to you!',
+		'Happy Birthday, genius! Hope it\'s the best one yet.',
+		'You rock. You are brilliant. I\'ve said enough. My warmest wishes!',
+		'Dang you are OLD. Happy birthday anyway!',
+		'Beep Boop. I\'m going to wish you a birthday. Happy Birthday!',
+		'No matter how old you get, you will always be MY +1. Happy Birthday!',
+		'Wow! You\'ve still got all your teeth! Happy Birthday!',
+		'Let the party begin, Happy Birthday!'
+		]
+
+	if 'happy birthday' in message.content.lower():
+		await channel.send(random.choice(happyBirthdayMessages))
+
+@bot.event
 async def on_member_join(member):
 	channel = bot.get_channel(int(os.getenv('DISCORD_CHANNEL')))
 	await channel.send(f'Welcome, {member.name}! \nThe current Among Us room is #{amongUsRoom}')
