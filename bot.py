43bed6c94651211e1109f6167436e749139a4b87
MasterApplesauc <joe.vincent96@gmail.com>
2021-03-03 11:50:48 -0800

    Attempted to add a DM for new users - incomplete

--- bot.py
+++ bot.py
@@ -4,22 +4,19 @@ import discord
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	for guild in client.guilds:
-		if guild.name == GUILD:
-			break
+	print(f'{client.user.name} has connected to Discord!')
 
-	print(
-		f'{client.user} is connected to the following guild:\n'
-		f'{guild.name} (id: {guild.id})\n'
+@client.event
+async def on_member_join(member):
+	print(member)
+	await member.create_dm()
+	await member.dm_channel.send(
+		f'Hi {member.name}, welcome to my Discord server!'
 	)
-
-	members = '\n - '.join([member.name for member in guild.members])
-	print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
